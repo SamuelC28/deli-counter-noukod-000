4f5c0170f2c5f8eac89119e2c60a0ebf5e62e34a
Samuel Cornet <samuel@noukod.com>
2018-09-19 19:14:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,19 @@
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+
+end
+
 def line(katz_deli)
-  if katz_deli.length < 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     message="The line is currently:"
@@ -9,4 +23,4 @@ def line(katz_deli)
     end
 
     puts "#{message}"
-  end
+end
